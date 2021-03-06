7ec3ba285b4d132037f85bc6c6a52a903f47a045
李俊諭 JYUN-YU LI <s123600g@gmail.com>
2021-03-06 23:21:20 +0800

    feat: Create project initialize files

---
+++ Jyu.Sample.Kafka/Jyu.Sample.Kafka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jyu.Sample.Kafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
